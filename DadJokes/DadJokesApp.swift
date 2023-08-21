c60811d1b8de14192678c7f06b1c2ea710c81596
Tiffany Liu <mazingmonkey2004@gmail.com>
2023-08-21 14:46:38 -0400

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Scholar on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
